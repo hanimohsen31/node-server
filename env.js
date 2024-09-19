7ef040913f52ecfa2f6ba761555d6bbc591ad054
Hani Rashed <hanimohsen31@gmail.com>
2024-09-19 21:41:55 +0300

    remove secret

--- env.js
+++ env.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv')
 dotenv.config({ path: './.env' }) // environment variables
 
 const env = {
-  MONGO_CONNECT_URI: process.env.MONGO_CONNECT_URI || 'mongodb+srv://hanimohsen3131:npccNFit7CD7eOea@cluster0.dsyn0.mongodb.net/',
-  COLLECTION: process.env.COLLECTION || 'traveller',
-  JWT_KEY: process.env.JWT_KEY || 'onix-3810-this-may-be-the-secret-of-my-application',
-  EXPIRATION: process.env.EXPIRATION || '30d',
+  MONGO_CONNECT_URI: process.env.MONGO_CONNECT_URI,
+  COLLECTION: process.env.COLLECTION,
+  JWT_KEY: process.env.JWT_KEY,
+  EXPIRATION: process.env.EXPIRATION,
 }
 
 module.exports = env
