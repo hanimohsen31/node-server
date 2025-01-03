ee3245f8fdaed954c2c2b217911abdcde846dbf9
Hani Mohsen <hanimohsen31@gmail.com>
2025-01-03 03:53:10 +0200

    aa

--- controllers/blogging-controller.js
+++ controllers/blogging-controller.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Blogging = require('../modals/blogging-modal')
 const ErrorHandler = require('../utils/ErrorHandler')
+const axios = require('axios');
 
 // create
 async function CreateBlog(req, res) {
@@ -63,8 +64,39 @@ async function DeleteBlog(req, res) {
   }
 }
 
+async function AddPostToBLog(req, res) {
+  let reqQuery = { ...req.query }
+  try {
+    // method 1 for filter
+    let blog = await Blogging.findOne(reqQuery)
+
+    let payload = {
+      kind: 'blogger#post',
+      blog: { id: '8070105920543249955' },
+      title: blog.postTitle,
+      content: blog.blogPost,
+    }
+    
+    // Send post request to Blogger API
+    const response = await axios.post(
+      'https://www.googleapis.com/blogger/v3/blogs/8070105920543249955/posts/',
+      payload,
+      {
+        headers: {
+          Authorization: `Bearer ${'110183634560-skcsgkdaaqkopl5241gb6bvttda1k255.apps.googleusercontent.com'}`,
+        },
+      }
+    );
+
+    res.status(200).json({ message: 'Blog', data: blog })
+  } catch (err) {
+    res.status(418).json({ message: 'No Tours Found', data: null, error: err })
+  }
+}
+
 router.route('').post(CreateBlog).get(GetAllBlogs)
 router.route('/order').get(GetInOrderData)
 router.route('/:id').delete(DeleteBlog)
 router.route('/isPublished/:id').patch(UpdateIsPublished)
+router.route('/blog').patch(UpdateIsPublished)
 module.exports = router
